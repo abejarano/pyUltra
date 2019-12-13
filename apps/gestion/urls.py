063fabe135701f0bcf4c58726ec98be4894ce08f
angel <angel@alphacode.com.br-at-503688510464>
2019-12-13 07:32:05 -0300

    mas vistas integradas con sus rutas

--- apps/gestion/urls.py
+++ apps/gestion/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path('tenedores/registrar', TenedoresRegistrar.as_view()),
+    path('tenedores/listado', TenedoresListado.as_view()),
 
+    path('productos/registrar', ProductosRegistrar.as_view()),
+    path('productos/listado', ProductosListado.as_view()),
+
+    path('denuncias/registrar', DenunciasRegistrar.as_view()),
+    path('denuncias/listado', DenunciasListado.as_view()),
 ]
--- apps/gestion/views.py
+++ apps/gestion/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+class TenedoresListado(TemplateView):
+    template_name = 'gestion/tenedores-listado.html'
+
+class TenedoresRegistrar(TemplateView):
+    template_name = 'gestion/tenedores-registrar.html'
+
+class ProductosRegistrar(TemplateView):
+    template_name = 'gestion/productos-registrar.html'
+
+class ProductosListado(TemplateView):
+    template_name = 'gestion/productos-listado.html'
+
+class DenunciasRegistrar(TemplateView):
+    template_name = 'gestion/denuncias-registrar.html'
+
+class DenunciasListado(TemplateView):
+    template_name = 'gestion/denuncias-listado.html'
