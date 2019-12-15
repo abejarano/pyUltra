4bf9019377bcf0c2c42e133733f7fd28cc52ce56
angel <angel@alphacode.com.br-at-503688510464>
2019-12-15 11:37:08 -0300

    rutas y archivos html para la seccion de categorias

--- apps/categorias/urls.py
+++ apps/categorias/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-
+    path('clases/listado', ClasesListado.as_view()),
+    path('clases/registrar', ClasesRegistrar.as_view()),
+    path('modalidades/listado', ModalidadListado.as_view()),
+    path('modalidades/registrar', ModalidadRegitrar.as_view()),
+    path('nacionalidades/listado', NacionalidadesListado.as_view()),
+    path('nacionalidades/registrar', NacionalidadesRegistrar.as_view()),
+    path('situaciones/listado', SituacionesListado.as_view()),
+    path('situaciones/registrar', SituacionesRegistrar.as_view()),
+    path('tiendas/listado', TiendasListado.as_view()),
+    path('tiendas/registrar', TiendasRegistrar.as_view()),
+    path('tipo-involucrados/listado', TipoInvolucaradosListado.as_view()),
+    path('tipo-involucrados/registrar', TipoInvolucaradosRegistrar.as_view()),
+    path('tipo-procesos/listado', TipoProcesosListado.as_view()),
+    path('tipo-procesos/registrar', TipoProcesosRegistrar.as_view()),
 ]
--- apps/categorias/views.py
+++ apps/categorias/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class ClasesListado(TemplateView):
+    template_name = 'categorias/clases-listado.html'
+
+class ClasesRegistrar(TemplateView):
+    template_name = 'categorias/clases-registrar.html'
+
+class ModalidadListado(TemplateView):
+    template_name = 'categorias/modalidad-listado.html'
+
+class ModalidadRegitrar(TemplateView):
+    template_name = 'categorias/modalidad-registrar.html'
+
+class NacionalidadesListado(TemplateView):
+    template_name = 'categorias/nacionalidades-listado.html'
+
+class NacionalidadesRegistrar(TemplateView):
+    template_name = 'categorias/nacionalidades-registrar.html'
+
+class SituacionesListado(TemplateView):
+    template_name = 'categorias/situaciones-listado.html'
+
+class SituacionesRegistrar(TemplateView):
+    template_name = 'categorias/situaciones-registrar.html'
+
+class TiendasListado(TemplateView):
+    template_name = 'categorias/tiendas-listado.html'
+
+class TiendasRegistrar(TemplateView):
+    template_name = 'categorias/tiendas-registrar.html'
+
+class TipoInvolucaradosListado(TemplateView):
+    template_name = 'categorias/tipo_involucrados-listado.html'
+
+class TipoInvolucaradosRegistrar(TemplateView):
+    template_name = 'categorias/tipo_involucrados-registrar.html'
+
+class TipoProcesosListado(TemplateView):
+    template_name = 'categorias/tipo_procesos-listado.html'
+
+class TipoProcesosRegistrar(TemplateView):
+    template_name = 'categorias/tipo_procesos-registrar.html'
