eab3660ba4ed61abd9be1d6706a27e744f382bf3
angel <angel@alphacode.com.br-at-503688510464>
2020-04-12 21:27:51 -0300

    proteccion de url

--- apps/seguimiento/urls.py
+++ apps/seguimiento/urls.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 # from apps.seguimiento.views import generate_pdf
 from .views import *
 
 urlpatterns = [
-    path('intervencion/<pk>/registrar', IntervencionRegistrar.as_view()),
-    path('intervencion/<pk_denuncia>/listado', IntervencionListado.as_view()),
-    path('intervencion/<pk>/editar', IntervencionEditar.as_view())
+    path('intervencion/<pk>/registrar', login_required(IntervencionRegistrar.as_view())),
+    path('intervencion/<pk_denuncia>/listado', login_required(IntervencionListado.as_view())),
+    path('intervencion/<pk>/editar', login_required(IntervencionEditar.as_view())),
+    path('intervencion/<pk>/eliminar', login_required(IntervencionEliminar.as_view())),
 ]
