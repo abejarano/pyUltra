f0532dfaf7b1cb1e5ad741fc6fc317e240bd699c
angel <angel@alphacode.com.br-at-503688510464>
2020-06-19 19:46:56 -0300

    grafica

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('data', dataGrafico),
     path('registrar', login_required(Usuario_Registro.as_view())),
     path('listado', login_required(Usuario_Listado.as_view())),
     path('<pk>/eliminar', login_required(Usuario_Eliminar.as_view())),
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,21 +1,61 @@
+import json
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db import connection
+from django.db.models import Count, Func, F, Value, DateField
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
 # Create your views here.
+from django.utils.timezone import now
 from django.views.generic import TemplateView, CreateView, ListView, DeleteView
+from requests import Response
 
 from apps.gestion.models import Tenderos, Denuncias
 from apps.seguimiento.models import Intervenciones
 from apps.usuarios.forms import UserCreateForm
+import datetime
+from django.http import JsonResponse
+
+
+def dataGrafico(request):
+    now = datetime.datetime.now()
+    after = now - datetime.timedelta(days=7)
+    after = after.strftime("%Y-%m-%d")
+    now = now.strftime("%Y-%m-%d")
+    cursor = connection.cursor()
+
+    cursor = connection.cursor("select to_char(fecha_denuncia, 'DD-MM-YYYY') as fecha, count(id) as cantidad "
+                                "from gestion_denuncias "
+                               "WHERE to_char(fecha_denuncia, 'YYYY-MM-DD') BETWEEN '"+after+"' AND '"+now+"' "
+                                "GROUP BY to_char(fecha_denuncia, 'DD-MM-YYYY')")
+
+    #cursor.execute("select DATE_FORMAT(fecha_denuncia, '%d-%m-%Y') as fecha, count(id) as cantidad "
+    #               "from gestion_denuncias "
+    #               "WHERE DATE_FORMAT(fecha_denuncia, '%Y-%m-%d') BETWEEN '" + after + "' AND '" + now + "' "
+    #                "GROUP BY DATE_FORMAT(fecha_denuncia, '%d-%m-%Y')")
+    columns = [col[0] for col in cursor.description]
+    data = {}
+    data['Fecha'] = 'Cantidad de casos'
+    x = [dict(zip(columns, row))
+         for row in cursor.fetchall()
+         ]
+    for item in x:
+        data[item['fecha']] = item['cantidad']
+
+
+    return JsonResponse(json.loads(json.dumps(dict(data), cls=DjangoJSONEncoder)))
 
 def getTotalData():
     context = {}
     context['total_tenderos'] = Tenderos.objects.count()
     context['total_denuncias'] = Denuncias.objects.count()
     context['total_intervenciones'] = Intervenciones.objects.count()
+
+
     return context
 
 def Index(request):
@@ -50,7 +90,6 @@ def iniciar_sesion(request):
 def cerrar_sesion(request):
     logout(request)
     return redirect('/')
-
 
 class Usuario_Registro(CreateView):
     model = User
